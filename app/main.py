1ac143c6489a7ab8d34410b3018d16ff4c30497f
giu <giulidimasi@gmal.com>
2026-03-05 03:42:46 -0300

    serve_frontend

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from app.services.sentiment_service import predict_sentiment
 from app.schemas.request_schema import TextRequest
+import os
 
 app = FastAPI()
-
-@app.get('/')
-async def serve_frontend():
-    return FileResponse('static/index.html')
 
 app.add_middleware(CORSMiddleware,
                    allow_origins=["*"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"],)
+
+if os.path.exists("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.post('/coment')
 async def coment(request: TextRequest):
