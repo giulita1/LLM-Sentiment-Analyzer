df152976a431959e292746ef624615a20af98308
giu <giulidimasi@gmal.com>
2026-03-05 03:13:52 -0300

    serve frontend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from app.services.sentiment_service import predict_sentiment
 from app.schemas.request_schema import TextRequest
 
 app = FastAPI()
+
+@app.get('/')
+async def serve_frontend():
+    return FileResponse('static/index.html')
 
 app.add_middleware(CORSMiddleware,
                    allow_origins=["*"],
